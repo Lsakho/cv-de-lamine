f88eb2d75cd8e168751f1377047576e99c2b8891
Lamine Sakho <89203822+Lsakho@users.noreply.github.com>
2022-03-11 16:31:51 +0100

    add js
    i didn't menage to switch to the both themes

---
+++ main.js
@@ -0,0 +1,5 @@
+// // this function allows me to change the interface of my HTML page and put in dark mode
+function modenuit() {
+    document.getElementById("dark-mode").style.filter = "grayscale(100%)";
+    document.body.style.backgroundColor = "gray";
+}
